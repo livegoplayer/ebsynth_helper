710124f65e231d4bf9a87aa9fcd63c189317ceb0
xjyplayer <2532917800@qq.com>
2023-06-03 19:54:36 +0800

    添加人像mask

--- scripts/mod_helper.py
+++ scripts/mod_helper.py
@@ -1,11 +1,14 @@
 import cv2
+import numpy as np
+
 
 def infer2(img_path: str, mask_path: str = '', out_img_path: str = ''):
 
     # 使用opencv叠加图片
     img1 = cv2.imread(img_path)
     img2 = cv2.imread(mask_path)
-
-    image = cv2.add(img1, img2)
+    # 反转
+    matrix = 255 - np.asarray(img2)
+    image = cv2.add(img1, matrix)
     print("save image to " + out_img_path)
     cv2.imwrite(out_img_path, image)
